3ccd9a21bff0d3d50625c91d91bf7b63eeddd5f6
Santiago Sanchez <chagostwo@yahoo.com>
2019-06-07 03:20:58 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -2,7 +2,7 @@ class Book
   attr_accessor :author, :page_count
   attr_reader :title
 
-  GENRES = []
+  @@GENRES = []
 
   def initialize(title)
     @title = title
@@ -10,11 +10,14 @@ class Book
 
   def genre=(genre)
     @genre = genre
-    GENRES << genre
+    @@GENRES << genre
   end
 
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
 
+  def all
+    @@GENRES
+  end
 end
